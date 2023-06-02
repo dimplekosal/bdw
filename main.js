a9d628a8093e1147a112a87d53529e7593c7131d
dimplekosal <129039138+dimplekosal@users.noreply.github.com>
2023-06-02 18:34:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
     {
-        fabric.fromURL('BirthdayImage.jpg',function(Img)
+        fabric.Image.fromURL('BirthdayImage.jpg',function(Img)
         {block_image_object = Img;
         block_image_object.scaleToWidth(700);       
          block_image_object.scaleToHeight(500);
@@ -15,7 +15,7 @@ function new_image()
 });
 }
 
-function playSound()
+function playsound()
 {
 x.play();
     
